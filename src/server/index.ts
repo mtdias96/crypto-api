b15e58e08f8f89c3fa8239993efcd01babe292e0
mtdias96 <matheusdias.front96@gmail.com>
2024-05-08 16:07:24 -0300

    att test2

--- src/server/index.ts
+++ src/server/index.ts
@@ -11,8 +11,13 @@ app.use(express.json())
 app.post('/signup', routeAdapter(makeSignUpController()))
 app.post('/signin', routeAdapter(makeSignInController()))
 app.get('/test', (_, res) => {
-  res.send('O PAi ta onnn!');
-})
+  const responseBody = {
+    message: 'O PAi ta onnn!',
+    status: 'success',
+    data: { foo: 'bar' }
+  };
+  res.json(responseBody);
+});
 
 app.listen(3001, () => {
   console.log("Server started at http://localhost:3001");
